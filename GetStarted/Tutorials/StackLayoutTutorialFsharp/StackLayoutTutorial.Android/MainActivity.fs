cf421e98e9d00b0be6da5f6b69aa6dcba4e44e00
Sam Williams <sam.williams@Ashleys-MacBook-Pro.local>
2019-11-23 15:13:39 +1300

    Add StackLayoutTutorialFsharp

---
+++ GetStarted/Tutorials/StackLayoutTutorialFsharp/StackLayoutTutorial.Android/MainActivity.fs
@@ -0,0 +1,20 @@
+namespace StackLayoutTutorial.Droid
+
+open System
+open Android.App
+open Android.Content.PM
+open Android.Runtime
+open Android.Views
+open Android.Widget
+open Android.OS
+
+[<Activity(Label = "StackLayoutTutorial", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true,
+           ConfigurationChanges = (ConfigChanges.ScreenSize ||| ConfigChanges.Orientation))>]
+type MainActivity() =
+    inherit global.Xamarin.Forms.Platform.Android.FormsAppCompatActivity()
+    override this.OnCreate(savedInstanceState: Bundle) =
+        global.Xamarin.Forms.Platform.Android.FormsAppCompatActivity.TabLayoutResource <- Resources.Layout.Tabbar
+        global.Xamarin.Forms.Platform.Android.FormsAppCompatActivity.ToolbarResource <- Resources.Layout.Toolbar
+        base.OnCreate(savedInstanceState)
+        global.Xamarin.Forms.Forms.Init(this, savedInstanceState)
+        base.LoadApplication(new StackLayoutTutorial.App())
---
+++ GetStarted/Tutorials/StackLayoutTutorialFsharp/StackLayoutTutorial.Android/Properties/AssemblyInfo.fs
@@ -0,0 +1,23 @@
+namespace StackLayoutTutorial.Droid
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+open Android.App
+
+type Resources = StackLayoutTutorial.Droid.Resource
+[<assembly: Android.Runtime.ResourceDesigner("StackLayoutTutorial.Droid.Resources", IsApplication=true)>]
+
+[<assembly:AssemblyTitle("StackLayoutTutorial.Android")>]
+[<assembly:AssemblyDescription("")>]
+[<assembly:AssemblyConfiguration("")>]
+[<assembly:AssemblyCompany("")>]
+[<assembly:AssemblyProduct("StackLayoutTutorial.Android")>]
+[<assembly:AssemblyCopyright("Copyright ©  2014")>]
+[<assembly:AssemblyTrademark("")>]
+[<assembly:AssemblyCulture("")>]
+[<assembly:ComVisible(false)>]
+[<assembly:AssemblyVersion("1.0.0.0")>]
+[<assembly:AssemblyFileVersion("1.0.0.0")>]
+[<assembly:UsesPermission(Android.Manifest.Permission.Internet)>]
+[<assembly:UsesPermission(Android.Manifest.Permission.WriteExternalStorage)>]
+do ()
